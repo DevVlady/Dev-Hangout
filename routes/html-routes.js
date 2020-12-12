2caa49f1b6ac49d237916f86cb7f0d2408b273bc
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-12-12 11:11:20 -0600

    Fixed the continue button path on the index.html file to the signup html when users cvlicks continut.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -25,12 +25,13 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
+    console.log('**MEMBERS.HTML**')
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
 
   //THIS IS THE ROUTE INDEX. THE HOMEPAGE. FIRST PAGE YOU WILL SEE
   app.get("/", (req, res) => {
-    console.log('**PROFILE.HTML**')
+    console.log('**ROOT.HTML**')
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
@@ -39,7 +40,7 @@ module.exports = function(app) {
   //   res.sendFile(path.join(__dirname, "../public/login.html"));
   // });
   app.get("/signup", (req, res) => {
-    console.log('**PROFILE.HTML**')
+    console.log('**SIGNUP.HTML**')
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
--- server.js
+++ server.js
@@ -28,5 +28,3 @@ db.sequelize.sync({force: false}).then(function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
-
-//michaelbartek commit
