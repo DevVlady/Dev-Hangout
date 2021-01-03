25ab4b1ad70023d840b1038ebc6d1f6d57ed83bc
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-01-02 20:21:00 -0600

    Fixed the github api routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,40 +2,47 @@
 var db = require("../models");
 var passport = require("../config/passport");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    console.log("req.user :" , req.user)
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
+    console.log("req.user :", req.user);
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       username: req.body.username,
-      password: req.body.password
+      password: req.body.password,
+      githubname: req.body.githubname,
     })
-      .then(function() {
-        res.redirect(307, "/api/login");
+      .then(function (data) {
+        if (!data.githubname) {
+          console.log("redirect - (307) /api/login");
+          res.redirect(307, "/api/login");
+        } else {
+          console.log("redirect - /login/github");
+          res.redirect("/login/github");
+        }
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/login");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
@@ -45,9 +52,8 @@ module.exports = function(app) {
       res.json({
         email: req.user.email,
         username: req.user.username,
-        id: req.user.id
+        id: req.user.id,
       });
     }
   });
-
 };
