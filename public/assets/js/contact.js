bdd64557f3205beb775a782bbc6184f918d09b4f
Hoybaby <mbartek436@gmail.com>
2021-02-11 15:39:44 -0500

    cleaned up unnesccary code from contact.js

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -1,25 +1,3 @@
-// const btn = document.getElementById('button');
-
-// const { EmailJSResponseStatus } = require("emailjs-com");
-
-// document.getElementById('contact-form')
-//     .addEventListener('submit', function (event) {
-//         event.preventDefault();
-
-//         btn.value = 'Sending...';
-
-//         const serviceID = 'contact_service';
-//         const templateID = 'contact_form';
-
-//         emailjs.sendForm(serviceID, templateID, this)
-//             .then(() => {
-//                 btn.value = 'Send Email';
-//                 alert('Sent!');
-//             }, (err) => {
-//                 btn.value = 'Send Email';
-//                 alert(JSON.stringify(err));
-//             });
-//     });
 
 function sendMail(params) {
     let tempParams = {
@@ -34,13 +12,3 @@ function sendMail(params) {
         })
 }
 
-// function sendEmail(e) {
-//     e.preventDefault();
-
-//     emailjs.sendForm('contact_service', 'contact_form', e.target, 'user_uUtrRyGsoUMd0aTdjzprd')
-//         .then((result) => {
-//             console.log(result.text);
-//         }, (error) => {
-//             console.log(error.text);
-//         });
-// }
